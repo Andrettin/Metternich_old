e4bde4ad221b67e3273734573f9f6ca7fe0d6bd8
Andrettin <andre.ng@live.com>
2019-07-27 07:46:43 +0200

    Load the province database when starting the engine

--- main.cpp
+++ main.cpp
@@ -1,11 +1,20 @@
+#include "province.h"
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+
+static void LoadDatabase()
+{
+	Province::LoadDatabase();
+}
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
 	QGuiApplication app(argc, argv);
+
+	LoadDatabase();
 
 	QQmlApplicationEngine engine;
 	const QUrl url(QStringLiteral("qrc:/main.qml"));
