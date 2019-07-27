393c08b7933210e236957f03fd67a01b4d075746
Andrettin <andre.ng@live.com>
2019-07-27 18:04:08 +0200

    Import main.qml from the working directory instead of compiling it together with the program

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 	}
 
 	QQmlApplicationEngine engine;
-	const QUrl url(QStringLiteral("qrc:/main.qml"));
+	const QUrl url(QStringLiteral("./main.qml"));
 	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app, [url](QObject *obj, const QUrl &objUrl) {
 		if (!obj && url == objUrl) {
 			QCoreApplication::exit(-1);
