beb5bef7341f86bb62473252d3afd8bc8f4aad02
Andrettin <andre.ng@live.com>
2019-11-07 21:46:10 +0100

    Holding types now have a "slot_type" property

--- holding/holding_type.h
+++ holding/holding_type.h
@@ -10,12 +10,13 @@ namespace metternich {
 
 class building;
 class modifier;
+enum class holding_slot_type : int;
 
 class holding_type : public data_entry, public data_type<holding_type>
 {
 	Q_OBJECT
 
-	Q_PROPERTY(bool settlement MEMBER settlement READ is_settlement)
+	Q_PROPERTY(metternich::holding_slot_type slot_type MEMBER slot_type READ get_slot_type)
 
 public:
 	static constexpr const char *class_identifier = "holding_type";
@@ -26,9 +27,9 @@ public:
 
 	virtual void process_gsml_scope(const gsml_data &scope) override;
 
-	bool is_settlement() const
+	holding_slot_type get_slot_type() const
 	{
-		return this->settlement;
+		return this->slot_type;
 	}
 
 	const std::vector<building *> &get_buildings() const
@@ -52,7 +53,7 @@ public:
 	}
 
 private:
-	bool settlement = false;	//whether the holding type occupies a settlement slot
+	holding_slot_type slot_type;	//the slot type which the holding type occupies
 	std::vector<building *> buildings;
 	std::unique_ptr<metternich::modifier> modifier; //the modifier applied to holdings of this type
 };
