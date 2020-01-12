2a4b24778eb4528b8b6a5c2555b71d045b2b5141
Andrettin <andre.ng@live.com>
2020-01-12 14:38:32 +0100

    Added support for trade routes having path branches

--- economy/trade_route.cpp
+++ economy/trade_route.cpp
@@ -3,6 +3,7 @@
 #include "map/province.h"
 #include "map/world.h"
 #include "util/container_util.h"
+#include "util/map_util.h"
 
 namespace metternich {
 
@@ -12,9 +13,28 @@ void trade_route::process_gsml_scope(const gsml_data &scope)
 
 	if (tag == "path") {
 		this->clear_path();
+
+		std::vector<province *> path;
+
 		for (const std::string &province_identifier : scope.get_values()) {
 			province *path_province = province::get(province_identifier);
-			this->add_path_province(path_province);
+			path.push_back(path_province);
+		}
+
+		this->add_path_branch(path);
+
+	} else if (tag == "path_branches") {
+		this->clear_path();
+
+		for (const gsml_data &path_branch_data : scope.get_children()) {
+			std::vector<province *> path_branch;
+
+			for (const std::string &province_identifier : path_branch_data.get_values()) {
+				province *path_province = province::get(province_identifier);
+				path_branch.push_back(path_province);
+			}
+
+			this->add_path_branch(path_branch);
 		}
 	} else {
 		data_entry_base::process_gsml_scope(scope);
@@ -31,25 +51,32 @@ void trade_route::initialize()
 		throw std::runtime_error("The path of trade route \"" + this->get_identifier() + "\" consists of only one province.");
 	}
 
-	for (province *path_province : this->path) {
+	for (const auto &kv_pair : this->path) {
+		province *path_province = kv_pair.first;
 		if (path_province == nullptr) {
 			throw std::runtime_error("The path of trade route \"" + this->get_identifier() + "\" contains a null province.");
 		}
 
 		path_province->add_trade_route(this);
+
+		trade_route_path_element *path_element = kv_pair.second.get();
+		if (path_element->get_previous().empty() || path_element->get_next().empty()) {
+			this->path_endpoints.insert(path_province);
+		}
 	}
 
-	province *start_province = this->path.front();
+	const province *endpoint_province = *this->path_endpoints.begin();
 
 	if (this->get_world() == nullptr) {
-		this->set_world(start_province->get_world());
+		this->set_world(endpoint_province->get_world());
 	}
 
 	if (!this->get_rect().isValid()) {
 		QPoint top_left(-1, -1);
 		QPoint bottom_right(-1, -1);
 
-		for (province *path_province : this->path) {
+		for (const auto &kv_pair : this->path) {
+			const province *path_province = kv_pair.first;
 			const QPoint province_pos = path_province->get_center_pos();
 
 			if (top_left.x() == -1 || province_pos.x() < top_left.x()) {
@@ -85,28 +112,58 @@ void trade_route::set_world(metternich::world *world)
 
 QVariantList trade_route::get_path_qvariant_list() const
 {
-	return container::to_qvariant_list(this->path);
+	return container::to_qvariant_list(map::get_keys(this->path));
 }
 
-void trade_route::add_path_province(province *path_province)
+void trade_route::add_path_branch(const std::vector<province *> &path_branch)
 {
-	this->path.push_back(path_province);
+	std::vector<const province *> path_branch_provinces;
+
+	for (size_t i = 0; i < path_branch.size(); ++i) {
+		province *path_province = path_branch[i];
+
+		trade_route_path_element *path_element = nullptr;
+		auto find_iterator = this->path.find(path_province);
+		if (find_iterator != this->path.end()) {
+			path_element = find_iterator->second.get();
+		} else {
+			auto new_path_element = std::make_unique<trade_route_path_element>(path_province);
+			path_element = new_path_element.get();
+			this->path[path_province] = std::move(new_path_element);
+		}
+
+		if (i > 0) {
+			province *previous_path_province = path_branch[i - 1];
+			path_element->add_to_previous(this->path[previous_path_province].get());
+		}
+
+		path_branch_provinces.push_back(path_province);
+	}
+
+	this->path_branch_provinces.push_back(std::move(path_branch_provinces));
 }
 
 void trade_route::clear_path()
 {
 	this->path.clear();
+	this->path_endpoints.clear();
 }
 
-QVariantList trade_route::get_path_points_qvariant_list() const
+QVariantList trade_route::get_path_branch_points_qvariant_list() const
 {
-	QVariantList path_points;
+	QVariantList path_points_qvariant_list;
 
-	for (province *path_province : this->path) {
-		path_points.append(path_province->get_center_pos() - this->get_rect().topLeft());
+	for (const std::vector<const province *> &path_branch_provinces : this->path_branch_provinces) {
+		QVariantList path_branch_points_qvariant_list;
+
+		for (const province *path_province : path_branch_provinces) {
+			path_branch_points_qvariant_list.append(path_province->get_center_pos() - this->get_rect().topLeft());
+		}
+
+		path_points_qvariant_list.push_back(path_branch_points_qvariant_list);
 	}
 
-	return path_points;
+	return path_points_qvariant_list;
 }
 
 void trade_route::set_active(const bool active)
@@ -118,7 +175,8 @@ void trade_route::set_active(const bool active)
 	this->active = active;
 	emit active_changed();
 
-	for (province *path_province : this->path) {
+	for (const auto &kv_pair : this->path) {
+		province *path_province = kv_pair.first;
 		if (active) {
 			path_province->add_active_trade_route(this);
 		} else {
@@ -130,9 +188,11 @@ void trade_route::set_active(const bool active)
 void trade_route::calculate_active()
 {
 	//the endpoints for a trade route must have an owner (i.e. be settled) for the trade route to be active
-	if (this->path.front()->get_owner() == nullptr || this->path.back()->get_owner() == nullptr) {
-		this->set_active(false);
-		return;
+	for (const province *endpoint_province : this->path_endpoints) {
+		if (endpoint_province->get_owner() == nullptr) {
+			this->set_active(false);
+			return;
+		}
 	}
 
 	this->set_active(true);
--- economy/trade_route.h
+++ economy/trade_route.h
@@ -10,12 +10,49 @@ namespace metternich {
 class province;
 class world;
 
+class trade_route_path_element
+{
+public:
+	trade_route_path_element(province *province) : province(province)
+	{
+	}
+
+	province *get_province() const
+	{
+		return this->province;
+	}
+
+	const std::set<trade_route_path_element *> &get_previous() const
+	{
+		return this->previous;
+	}
+
+	void add_to_previous(trade_route_path_element *element)
+	{
+		this->previous.insert(element);
+
+		if (!element->next.contains(this)) {
+			element->next.insert(this);
+		}
+	}
+
+	const std::set<trade_route_path_element *> &get_next() const
+	{
+		return this->next;
+	}
+
+private:
+	province *province = nullptr; //the province for this path element
+	std::set<trade_route_path_element *> previous; //path elements immediately preceding this one
+	std::set<trade_route_path_element *> next; //path elements immediately succeeding this one
+};
+
 class trade_route : public data_entry, public data_type<trade_route>
 {
 	Q_OBJECT
 
 	Q_PROPERTY(QVariantList path READ get_path_qvariant_list CONSTANT)
-	Q_PROPERTY(QVariantList path_points READ get_path_points_qvariant_list CONSTANT)
+	Q_PROPERTY(QVariantList path_branch_points READ get_path_branch_points_qvariant_list CONSTANT)
 	Q_PROPERTY(QRect rect MEMBER rect READ get_rect CONSTANT)
 	Q_PROPERTY(bool active READ is_active NOTIFY active_changed)
 
@@ -37,14 +74,14 @@ public:
 
 	QVariantList get_path_qvariant_list() const;
 
-	void add_path_province(province *path_province);
+	void add_path_branch(const std::vector<province *> &path_branch);
 	void clear_path();
 
-	QVariantList get_path_points_qvariant_list() const;
+	QVariantList get_path_branch_points_qvariant_list() const;
 
 	bool is_endpoint(const province *province) const
 	{
-		return path.front() == province || path.back() == province;
+		return this->path_endpoints.contains(province);
 	}
 
 	const QRect &get_rect() const
@@ -65,7 +102,9 @@ signals:
 
 private:
 	world *world = nullptr;
-	std::vector<province *> path;
+	std::map<province *, std::unique_ptr<trade_route_path_element>> path;
+	std::set<const province *> path_endpoints;
+	std::vector<std::vector<const province *>> path_branch_provinces; //used to draw the trade route
 	QRect rect;
 	bool active = false; //whether the trade route is active
 };
