01389e57326efac67f87f0cf4f67503edaa9acbe
Andrettin <andre.ng@live.com>
2020-02-01 09:59:46 +0100

    Added utility functions to remove all of a given element from a vector, or just one

--- util/vector_util.h
+++ util/vector_util.h
@@ -5,11 +5,23 @@
 namespace metternich::vector {
 
 template <typename T, typename U>
-void merge(std::vector<T> &container, const U &other_container)
+void merge(std::vector<T> &vector, const U &other_container)
 {
 	for (const typename U::value_type &element : other_container) {
-		container.push_back(element);
+		vector.push_back(element);
 	}
+}
+
+template <typename T>
+inline void remove(T &vector, const typename T::value_type &element)
+{
+	vector.erase(std::remove(vector.begin(), vector.end(), element), vector.end());
+}
+
+template <typename T>
+inline void remove_one(T &vector, const typename T::value_type &element)
+{
+	vector.erase(std::find(vector.begin(), vector.end(), element));
 }
 
 }
