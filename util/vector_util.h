605a4289d1e2a3f07fad1e15619b5daa29431446
Andrettin <andre.ng@live.com>
2020-03-11 17:46:19 +0100

    Added vector::contains utility function

--- util/vector_util.h
+++ util/vector_util.h
@@ -5,6 +5,12 @@
 namespace metternich::vector {
 
 template <typename T, typename U>
+bool contains(const std::vector<T> &vector, const U &value)
+{
+	return std::find(vector.begin(), vector.end(), value) != vector.end();
+}
+
+template <typename T, typename U>
 void merge(std::vector<T> &vector, const U &other_container)
 {
 	for (const typename U::value_type &element : other_container) {
